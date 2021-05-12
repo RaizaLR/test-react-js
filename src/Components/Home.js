39f036f791b66c3bd94136206ebde7a4a42c6c78
RaizaLR <raizalongart@gmail.com>
2021-05-12 16:56:43 -0400

    aplicando identacion a pagina de empleos

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import {
     Link 
      } from "react-router-dom";
     
-    const Home = () => {
+    const Home = () => { // vista principal de botón para ver empleos
         return (
             <div className="home-page-container">    
                 <Link to="/jobs"><button className="view-jobs-button">Ver empleos</button></Link>
--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -1,18 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
-const Jobs = () => {
+const Jobs = () => { //listado de empleos
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get('https://jobs.github.com/positions.json?description=api')
+      .get('https://jobs.github.com/positions.json?description=api') // función realiza la petición get
       .then(res => {
-        setData(res.data);
+        setData(res.data); // cuando termina toma el resultado y actualiza el estado de data
       })
-  }, []);
+  }, []); 
 
-  return <div>{data.map(item=>(<div key={item.id} className="githubJobs"><div><b>Title:</b> {item.title}</div><div><b>Company:</b> {item.company}</div></div>))}</div>
+  return <div>{data.map(item=>(
+  <div key={item.id} className="githubJobs">
+      <div><b>Title:</b> {item.title}</div>
+      <div><b>Company:</b> {item.company}</div>
+  </div>))}</div>  // funcion recorre con map la data y luego imprime los titulos y las compañias de los empleos
+   
 };
 
 export default Jobs;
